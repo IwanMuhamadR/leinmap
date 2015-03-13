e71f10abb181ca1d0603cba8e2d2bb9bbdf0ecd9
okigaru <garu.okigaru@gmail.com>
2015-03-13 07:39:43 +0700

    Iwan

--- leinmap.sql
+++ leinmap.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2015 at 05:19 AM
+-- Generation Time: Mar 12, 2015 at 04:04 AM
 -- Server version: 5.5.32
 -- PHP Version: 5.4.16
 
@@ -21,6 +21,27 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `leinmap` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `leinmap`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE IF NOT EXISTS `messages` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `content` text NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `name`, `email`, `content`) VALUES
+(1, 'Iwan Muhamad Ridwannuloh', 'im_ridwannuloh@yahoo.com', 'test 123 qwerty asdfg zxcvb');
 
 -- --------------------------------------------------------
 
